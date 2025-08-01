f3f740bd7d3cd57d183f6b53d6f39e9296b6abb4
strawberry2020 <1556095513@qq.com>
2025-08-01 22:33:00 +0800

    实现 物料拖拽

--- src/editor/materials/Page/index.tsx
+++ src/editor/materials/Page/index.tsx
@@ -1,14 +1,28 @@
+import { message } from "antd";
 import type { PropsWithChildren } from "react";
+import { useDrop } from "react-dnd";
 
 function Page({ children }: PropsWithChildren) {
 
-  return (
-    <div
-      className='p-[20px] h-[100%] box-border'
-    >
-      {children}
-    </div>
-  )
+    const [{ canDrop }, drop] = useDrop(() => ({
+        accept: ['Button', 'Container'],
+        drop: (item: { type: string}) => {
+            message.success(item.type)
+        },
+        collect: (monitor) => ({
+          canDrop: monitor.canDrop(),
+        }),
+    }));
+
+    return (
+        <div
+            ref={(node)=>{drop(node)}}
+            className='p-[20px] h-[100%] box-border'
+            style={{ border: canDrop ? '2px solid blue' : 'none' }}
+        >
+            {children}
+        </div>
+    )
 }
 
 export default Page;
